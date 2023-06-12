c386b2d29926c1aeefb4426323203ff89a4fc5d6
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-06-12 21:53:37 +0100

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const item = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+<a class="gallery__link" href = ${original}>
+<img class="gallery__image" 
+src = ${preview} 
+data-source = ${original} 
+ alt = ${description}/>
+ </a>
+</li>`
+);
+
+gallery.insertAdjacentHTML("beforeend", item.join(""));
+
+gallery.addEventListener("click", modaleShow);
+function modaleShow(event) {
+  console.log("modal image is open");
+  event.preventDefault();
+  const { target } = event;
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const imgEl = target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `<img src="${imgEl}" width="800" height="600">`
+  );
+
+  instance.show();
+
+  if (instance.visible()) {
+    window.addEventListener("keydown", modaleClose);
+  }
+  function modaleClose(event) {
+    console.log("modal image is closed");
+
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
